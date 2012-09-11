0165f735e5e284b82d69b0aadbd6da3cb32b879d
John Ewart <john@unixninjas.org>
2012-09-11 10:41:19 -0700

    Updated fixpermissions.sh

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+# This was lifted from somewhere on the internet, and modified to work with Apple's stat tool
+
 TREE1="$1"
 TREE2="$2"
 stat='/usr/bin/stat -r'
-cut='cut -f 3,5,6 -d \\" \\"'
-echo "STAT: ${stat}"
 
 find "$TREE1" \( -type d -o -type f \) -print | \
 	while IFS= read -r src ; do
@@ -25,13 +25,13 @@ find "$TREE1" \( -type d -o -type f \) -print | \
 			g2=`echo "$3"`
 
 			if [ ${p1} != ${p2} ]; then
-				( echo old perms were ${p2} new perms are ${p1} ) #; set -x ; chmod "${p1}" "${tgt}" )
+				( echo old perms were ${p2} new perms are ${p1} ); set -x ; chmod "${p1}" "${tgt}" )
 			fi
 			if [ ${o1} != ${o2} ]; then
-				( echo old owner was ${o2} new owner is ${o1} ) #; set -x ; chown "${o1}" "${tgt}" )
+				( echo old owner was ${o2} new owner is ${o1} ); set -x ; chown "${o1}" "${tgt}" )
 			fi
 			if [ ${g1} != ${g2} ]; then
-				( echo old group was ${g2} new group is ${g1} ) #; set -x ; chgrp "${g1}" "${tgt}" )
+				( echo old group was ${g2} new group is ${g1} ); set -x ; chgrp "${g1}" "${tgt}" )
 			fi
 		fi
 	done
